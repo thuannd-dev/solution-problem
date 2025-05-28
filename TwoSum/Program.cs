30ab6a021844081b197a7276eb57d51b36dadc29
nguyenduongthuan75321@gmail.com <nguyenduongthuan75321@gmail.com>
2025-05-28 22:04:41 +0700

    5/28/2025

---
+++ TwoSum/Program.cs
@@ -0,0 +1,44 @@
+﻿class Solution
+{
+    public int[] TwoSumV1(int[] nums, int target)
+    {
+        for(int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (j == i) continue;
+                if (nums[i] + nums[j] == target)
+                {
+                    return [i, j];
+                }
+            }
+        }
+        return [];
+    }
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> numMap = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if(numMap.ContainsKey(complement))
+            {
+                return [numMap.GetValueOrDefault(complement), i];
+            }
+            numMap.TryAdd(nums[i], i);
+        }
+        return [];
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = [1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1];
+        int target = 11;
+        Solution solution = new Solution();
+        int[] result = solution.TwoSum(nums, target);
+        Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
+    }
+}
