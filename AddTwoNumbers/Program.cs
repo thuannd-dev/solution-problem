ab7fb663ccc422f3e21f5853fb4716e7203df4b5
nguyenduongthuan75321@gmail.com <nguyenduongthuan75321@gmail.com>
2025-05-29 19:48:49 +0700

    5/29/2025

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿// Fix for CS0246: Define the missing 'ListNode' class.  
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// Fix for CA1822: Mark 'AddTwoNumbers' as static since it does not access instance data.  
+public class Solution
+{
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry = 0)
+    {
+        if (l1 == null && l2 == null && carry == 0) return null;
+
+        int total = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
+        carry = total / 10;
+        return new ListNode(total % 10, AddTwoNumbers(l1?.next, l2?.next, carry));
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Example usage:
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode result = Solution.AddTwoNumbers(l1, l2);
+        // Print the result
+        while (result != null)
+        {
+            Console.Write(result.val + " ");
+            result = result.next;
+        }
+    }
+}
--- TwoSum/obj/Debug/net8.0/TwoSum.AssemblyInfo.cs
+++ TwoSum/obj/Debug/net8.0/TwoSum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TwoSum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30ab6a021844081b197a7276eb57d51b36dadc29")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwoSum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwoSum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
