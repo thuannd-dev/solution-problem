df0610a2ced29bed38904ae5e6897cd5d5c90a1c
nguyenduongthuan75321@gmail.com <nguyenduongthuan75321@gmail.com>
2025-09-17 16:48:40 +0700

    Solution PalindromeNumber and StringtoInteger(atoi) problem

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+
+        //var arrChar = x.ToString().ToCharArray();
+        //for (int i = 0; i < arrChar.Length / 2; i++)
+        //{
+        //    if (arrChar[i] != arrChar[arrChar.Length - i - 1])
+        //    {
+        //        return false;
+        //    }
+        //}
+        //return true;
+        int reverseNumber = 0, tmp = x;
+        while (x > 0) 
+        {
+            reverseNumber = reverseNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return reverseNumber == tmp;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Solution s = new Solution();
+        Console.WriteLine(s.IsPalindrome(121));
+    }
+
+}
---
+++ StringtoInteger(atoi)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+public class Solution
+{
+    public int MyAtoi(string s)
+    {
+        s = s.Trim();
+        if (s == "")
+        {
+            return 0;
+        }
+        var arrChar = s.ToCharArray();
+        int i;
+        if (!((arrChar[0] == '-' && arrChar.Length >= 2 && arrChar[1] != '+') ||
+                (arrChar[0] == '+' && arrChar.Length >= 2 && arrChar[1] != '-')))
+        {
+            return 0;
+        }
+        for (i = 1; i < arrChar.Length; i++)
+        {
+            if (!(arrChar[i] >= '0' && arrChar[i] <= '9'))
+            {
+                break;
+            }
+        }
+        var stringResult = "";
+        for (int j = 0; j < i; j++)
+        {
+            stringResult += arrChar[j];
+        }
+        Int32.TryParse(stringResult, out var result);
+        if (Regex.IsMatch(s, @"0000+") && result == 0)
+        {
+            return 0;
+        }
+        if (result == 0 && arrChar[0] == '-' && stringResult != "0" && stringResult != "")
+        {
+            return int.MinValue;
+        }
+        if (result == 0 && stringResult != "0" && stringResult != "")
+        {
+            return int.MaxValue;
+        }
+        return result;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Solution s = new Solution();
+        Console.WriteLine(s.MyAtoi("  -0012a42"));
+    }
+}
