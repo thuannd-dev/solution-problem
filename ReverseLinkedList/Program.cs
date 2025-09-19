5fbbee52754683f37981ee136afb8420c039ea67
nguyenduongthuan75321@gmail.com <nguyenduongthuan75321@gmail.com>
2025-09-20 01:42:45 +0700

    revert linked list

---
+++ ReverseLinkedList/Program.cs
@@ -0,0 +1,43 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode current = head;
+        Stack<int> st = new Stack<int>();
+        while (current != null)
+        {
+            st.Push(current.val);
+            current = current.next;
+        }
+        current = head;
+        while (current != null)
+        {
+            current.val = st.Pop();
+            current = current.next;
+        }
+        return head;
+    }
+
+}
