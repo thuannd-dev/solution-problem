97000592e687ee2ea9490acfb2f592ea060bc774
nguyenduongthuan75321@gmail.com <nguyenduongthuan75321@gmail.com>
2025-09-20 01:58:03 +0700

    Invert Binary Tree

---
+++ InvertBinaryTree/Program.cs
@@ -0,0 +1,27 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null)
+        {
+            return root;
+        }
+        TreeNode tmp = root.right;
+        root.right = InvertTree(root.left);
+        root.left = InvertTree(tmp);
+        return root;
+    }
+}
