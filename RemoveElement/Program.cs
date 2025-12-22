2247a849a3999ba30ea2d51a4c4b88b6e0ad7a8b
thuannd-dev <nguyenduongthuan75321@gmail.com>
2025-12-22 09:27:01 +0700

    https://leetcode.com/problems/remove-element

---
+++ RemoveElement/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int[] numsTemp = new int[nums.Length];
+        int result = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                numsTemp[result] = nums[i];
+                result++;
+            }
+        }
+        for (int i = 0; i < result; i++)
+        {
+            nums[i] = numsTemp[i];
+        }
+        return result;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        int[] nums = [3, 2, 2, 3];
+        Solution solution = new Solution();
+        int result = solution.RemoveElement(nums, 3);
+        Console.WriteLine(result);
+        Console.WriteLine("----------------");
+        for (int i = 0; i < result; i++) { 
+            Console.WriteLine(nums[i]);
+        }
+    }
+}
