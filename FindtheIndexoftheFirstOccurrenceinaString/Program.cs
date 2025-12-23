51ae6ee46d7c50ee2cce13190d8db6d835a38de3
thuannd-dev <nguyenduongthuan75321@gmail.com>
2025-12-23 15:25:04 +0700

    https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string

---
+++ FindtheIndexoftheFirstOccurrenceinaString/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        for(int i = 0; i <= haystack.Length - needle.Length; i++)
+        {
+            if(haystack.Substring(i, needle.Length) == needle)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Solution s = new Solution();
+        string haystack = "leetcode", needle = "leeto";
+        Console.WriteLine(s.StrStr(haystack, needle));
+    }
+}
