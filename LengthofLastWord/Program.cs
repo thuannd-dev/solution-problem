48c92257cc14b332b61b3ca4eb7488b693318b06
thuannd-dev <nguyenduongthuan75321@gmail.com>
2025-12-30 04:51:58 +0700

    https://leetcode.com/problems/length-of-last-word

---
+++ LengthofLastWord/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        return s.TrimEnd().Split(' ').Last().Length;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Solution s = new Solution();
+        var a = "   fly me   to   the moon  ";
+        Console.WriteLine(s.LengthOfLastWord(a));
+    }
+}
