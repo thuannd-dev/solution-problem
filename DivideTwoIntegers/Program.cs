9daa31d6cab15b6f28a98d9ca05671798c347ef3
thuannd-dev <nguyenduongthuan75321@gmail.com>
2026-01-04 15:18:42 +0700

    https://leetcode.com/problems/search-insert-position

---
+++ DivideTwoIntegers/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int Divide(int dividend, int divisor)
+    {
+        try { return dividend / divisor; } catch (Exception e) { return (dividend < 0 && divisor < 0 || dividend > 0 && divisor > 0) ? Int32.MaxValue : Int32.MinValue; }
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Solution s = new Solution();
+        int dividend = -2147483648, divisor = -1;
+        Console.WriteLine(s.Divide(dividend, divisor));
+    }
+}
---
+++ SearchInsertPosition/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++) if (nums[i] == target || nums[i] > target) return i;
+        return nums.Length;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        int[] nums = { 1, 3, 5, 6 };
+        Solution solution = new Solution();
+        Console.WriteLine(solution.SearchInsert(nums, 7));
+    }
+}
