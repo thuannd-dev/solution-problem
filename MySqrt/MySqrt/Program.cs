fb4d95446eadeb0ecaa0d9bb6811d41c1b9fe146
thuannd-dev <nguyenduongthuan75321@gmail.com>
2026-03-24 16:43:43 +0700

    https://leetcode.com/problems/sqrtx

---
+++ MySqrt/MySqrt/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int MySqrt(int x)
+    {
+        //int pow = 1;
+        //while (pow * pow <= x)
+        //{
+        //    pow++;
+        //}
+        //return pow - 1;
+        return (int)Math.Sqrt(x);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int x = 4;
+        int result = solution.MySqrt(x);
+        Console.WriteLine(result); // Output: 2
+    }
+}
